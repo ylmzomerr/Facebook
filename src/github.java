57b3217d39d6a73f1fe2f9c47946dd0ac74c0a09
Omer YILMAZ <56378540+ylmzomerr@users.noreply.github.com>
2019-11-09 17:22:09 -0500

    Update github.java

--- src/github.java
+++ src/github.java
@@ -2,5 +2,7 @@ public class github {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("This is git up project");
+         System.out.println("Hi");
+        System.out.println("This is git up project");
     }
 }
